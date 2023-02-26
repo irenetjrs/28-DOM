2dd3272bb6b78bdd2da383cf8aa39f90a97ff5fc
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-27 01:31:09 +0200

    task 3

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.querySelector('h2').nextElementSibling.textContent = 'DOM method descri
 // task 2
 
 console.log(document.querySelectorAll('a[href*="/ua"]'));
+
+// task 3
+
+let list = document.querySelector('ul');
+let li = document.createElement("li");
+let li2 = document.createElement("li");
+let li4 = document.createElement("li");
+
+li.innerHTML = 0;
+li.setAttribute('id', 'null');
+list.prepend(li);
+li2.innerHTML = 2;
+li2.setAttribute('id', 'second');
+document.getElementById('first').after(li2);
+li4.innerHTML = 4;
+li4.setAttribute('id', 'forth');
+list.appendChild(li4);
